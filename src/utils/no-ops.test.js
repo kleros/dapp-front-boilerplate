f9b3619d84929d4268a1fbe708ff156aba2473ed
epiqueras <epiquerass@gmail.com>
2018-01-23 20:37:01 -0500

    refactor(utils): remove unused utils

--- src/utils/no-ops.test.js
+++
@@ -1,7 +0,0 @@
-import { noOp, renderNull } from './no-ops'
-
-describe('noOp', () =>
-  it('does nothing.', () => expect(noOp()).toBe(undefined)))
-
-describe('renderNull', () =>
-  it('returns null.', () => expect(renderNull()).toBe(null)))
