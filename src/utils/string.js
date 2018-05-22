653ef42230f897b86042896e6f0b9571aa3d2bff
Klaudiusz Dembler <k.dembler@gmail.com>
2018-05-22 12:55:42 +0200

    feat(utils): add string shortening utility

--- src/utils/string.js
+++ src/utils/string.js
@@ -27,3 +27,22 @@ export function camelToTitleCase(str) {
       p1 ? p1.toUpperCase() : p2 ? p2[0] + ' ' + p2[1] : ' ' + p3
   )
 }
+
+/**
+ * Shortens a string to a given length, by default hiding the middle of it. e.g. 'Hello World!', 7 => 'He...d!'
+ * @param {string} str - The string to shorten.
+ * @param {number} length - The length to which text should be shortened.
+ * @param {{position: string}} [options={position:middle}] - Object with an option determining which part of string should be removed during shortening.
+ * @returns {string} - The shortened string.
+ */
+export function shorten(str, length, { position = 'middle' } = {}) {
+  if (length >= str.length) return str
+  if (length < 5) return str.substr(0, length)
+
+  if (position === 'middle')
+    return `${str.substr(0, Math.floor(length / 2) - 1)}...${str.substr(
+      str.length - (Math.ceil(length / 2) - 2)
+    )}`
+  else if (position === 'right') return `${str.substr(0, length - 3)}...`
+  else return str.substr(0, length)
+}
